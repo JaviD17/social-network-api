ed67c63404b8b9820ee0c10daf7801ddd65a86ad
Javier Dominguez II <Javi7057@gmail.com>
2021-09-19 20:16:34 -0500

    server working and User model finished

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: "Email validation failed",
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
+// thoughts
+
+// Array of _id values referencing the Thought model
+// friends
+
+// Array of _id values referencing the User model (self-reference)
+// Schema Settings
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
