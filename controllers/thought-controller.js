0736b1f854b80d0875f761665a75688b70a95f98
Javier Dominguez II <Javi7057@gmail.com>
2021-09-19 22:38:32 -0500

    thought-controller layout set

---
+++ controllers/thought-controller.js
@@ -0,0 +1,13 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+    // get all thoughts
+
+    // get one thought by id
+
+    // create thought
+
+    // update thought by id
+
+    // delete thought
+}
